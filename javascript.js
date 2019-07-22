af35473d38fcab0e7a6f472d69e9440913a66293
Omoyi <urakhel7@gmail.com>
2019-07-22 13:15:08 +0200

    add 3 variables

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
+
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var gender = ["male", "female"];
+var male = ["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", "Kwasi",];
+var female = ["Adwoa", "Abenaa", "Akua","Yaa","Afua","Ama","Akosua",];
