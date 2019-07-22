4ce8fcc641a95f3c30cb8b40e30a35f936a8da0c
Omoyi <urakhel7@gmail.com>
2019-07-22 13:29:12 +0200

    add an if condition

--- javascript.js
+++ javascript.js
@@ -3,3 +3,7 @@ var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", 
 var gender = ["male", "female"];
 var male = ["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", "Kwasi",];
 var female = ["Adwoa", "Abenaa", "Akua","Yaa","Afua","Ama","Akosua",];
+ 
+if(dd>=0 && mm>=0 && yy>=0){
+    alert("your input is invalid!")
+};
